28b9f370a539943bf8200c629369c884f1d95e5d
张帅 <5346812+zhangshuaiii@user.noreply.gitee.com>
2022-10-27 17:21:59 +0800

    ts

--- src/StrUtil.php
+++ src/StrUtil.php
@@ -4,6 +4,10 @@ namespace Zsgogo\Test\utils;
 
 class StrUtil {
 
+    /**
+     * @param string $string
+     * @return false|string
+     */
     public function md5s(string $string){
         if ($string == "") return false;
         return md5(md5($string));
